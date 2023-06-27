866e2bc4eeee7b1597b90b24e786b9d1306d72c2
Vinleon Ang <javadripdev@gmail.com>
2023-06-27 21:14:13 +0800

    Add ToasterContext to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 
+import ToasterContext from "./context/ToasterContext";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -15,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ToasterContext />
+        {children}
+      </body>
     </html>
   );
 }
