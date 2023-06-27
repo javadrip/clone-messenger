0c445c2cab7f1ed41c918f5ca0ff44de4aedbee7
Vinleon Ang <javadripdev@gmail.com>
2023-06-27 12:55:06 +0800

    Create AuthSocialButton

---
+++ app/(site)/components/AuthSocialButton.tsx
@@ -0,0 +1,40 @@
+import { IconType } from "react-icons";
+
+interface AuthSocialButtonProps {
+  icon: IconType;
+  onClick: () => void;
+}
+
+const AuthSocialButton: React.FC<AuthSocialButtonProps> = ({
+  // Remap icon prop to Icon component that is used below
+  icon: Icon,
+  onClick,
+}) => {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className="
+        inline-flex
+        w-full
+        justify-center
+        rounded-md
+        bg-white
+        px-4
+        py-2
+        text-gray-500
+        shadow-sm
+        ring-1
+        ring-inset
+        ring-gray-300
+        hover:bg-gray-50
+        focus:outline-offset-0
+      "
+    >
+      {/* Component Icon is remapped from icon prop to Icon in line 10 */}
+      <Icon />
+    </button>
+  );
+};
+
+export default AuthSocialButton;
