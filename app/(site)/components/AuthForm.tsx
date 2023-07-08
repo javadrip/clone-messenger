ce475c941ec85f784071b72db52bd8759e74a1b4
Vinleon Ang <javadripdev@gmail.com>
2023-07-08 19:04:16 +0800

    Add credential check logic

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -5,6 +5,7 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { BsGithub, BsGoogle } from "react-icons/bs";
 import axios from "axios";
 import { toast } from "react-hot-toast";
+import { signIn } from "next-auth/react";
 
 import Input from "../../components/inputs/Input";
 import Button from "../../components/Button";
@@ -43,11 +44,25 @@ const AuthForm = () => {
       // /api/register because of folder structure
       axios
         .post("/api/register", data)
-        .catch(() => toast.error("Something went wrong"));
+        .catch(() => toast.error("Something went wrong"))
+        .finally(() => setIsLoading(false));
     }
 
     if (variant === "LOGIN") {
-      // NextAuth login
+      signIn("credentials", {
+        ...data,
+        redirect: false,
+      })
+        .then(callback => {
+          if (callback?.error) {
+            toast.error("Invalid credentials!");
+          }
+
+          if (callback?.ok && !callback?.error) {
+            toast.success("Logged in!");
+          }
+        })
+        .finally(() => setIsLoading(false));
     }
   };
 
