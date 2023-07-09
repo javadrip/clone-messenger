f36ed81ae7b5f45e134109f1ec52c7e9daf788df
Vinleon Ang <javadripdev@gmail.com>
2023-07-09 12:19:11 +0800

    Create middleware to protect users routes

---
+++ middleware.ts
@@ -0,0 +1,16 @@
+// Protect routes
+
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  pages: {
+    signIn: "/",
+  },
+});
+
+export const config = {
+  matcher: [
+    // Protects all of the users routes
+    "/users/:path*",
+  ],
+};
