d376dff836f0c615cb7a4bf4a42a19a11d7e0c6a
Vinleon Ang <javadripdev@gmail.com>
2023-08-10 15:55:31 +0800

    Create getUsers action

---
+++ app/actions/getUsers.ts
@@ -0,0 +1,31 @@
+import prisma from "@/app/libs/prismadb";
+import getSession from "./getSession";
+
+const getUsers = async () => {
+  const session = await getSession();
+
+  if (!session?.user?.email) {
+    return [];
+  }
+
+  try {
+    // Query list of users
+    const users = await prisma.user.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+      where: {
+        // Exclude self from the list
+        NOT: {
+          email: session.user.email,
+        },
+      },
+    });
+
+    return users;
+  } catch (error: any) {
+    return [];
+  }
+};
+
+export default getUsers;
