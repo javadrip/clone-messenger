975aecac3a58f48acbd771036773a954f3aa82a4
Vinleon Ang <javadripdev@gmail.com>
2023-08-12 20:28:59 +0800

    Remove console.log

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -16,8 +16,6 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
   const routes = useRoutes();
   const [isOpen, setIsOpen] = useState(false);
 
-  console.log("currentUser", currentUser);
-
   return (
     <div
       className="
